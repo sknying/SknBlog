a2612569165472de32fef211b675eb54ed997457
Sknying <sknying@gmail.com>
2026-07-14 08:07:00 +0800

    fix: allow lan origin in development

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  allowedDevOrigins: ["192.168.10.12"],
   images: {
     unoptimized: true,
     remotePatterns: [
